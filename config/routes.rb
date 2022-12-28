f74275ef7f2cadcdd10f7d75919e9b5c83d9d323
SONIDOBINARIO <SONIDOBINARIO@GMAIL.com>
2022-12-27 21:23:36 -0300

    Individual 1 m8 aplicado de estilo y modificado de vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/contact'
+  get 'home', to: 'pages#home'
+  get 'contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Nuestra ruta raiz del proyecto
+  root "pages#home"
 end
